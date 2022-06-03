6edf5d1277541dc1b9a8f081e6a1f89d0d5414e0
Anuroop <anuroopjoy@gmail.com>
2022-06-03 20:47:39 +0530

    base version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'standalone-app'`, () => {
+  it(`should have as title 'lazy-load-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('standalone-app');
+    expect(app.title).toEqual('lazy-load-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('standalone-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('lazy-load-app app is running!');
   });
 });
---
+++ src/app/loader/loader.interface.ts
@@ -0,0 +1,5 @@
+export interface Loader {
+  loader: any;
+  module?: string;
+  component?: string;
+}
